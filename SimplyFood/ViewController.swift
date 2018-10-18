2d3ba5d177dc5f482590e040211e677357fed4d1
Bjarke Høgdall <bjarkehogdall@bjarkes-macbook-pro.local>
2018-10-18 13:07:07 +0200

    Initial Commit

---
+++ SimplyFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimplyFood
+//
+//  Created by Bjarke Høgdall on 18/10/2018.
+//  Copyright © 2018 BjarkeHøgdall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
